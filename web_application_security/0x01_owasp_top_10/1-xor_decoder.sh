1c9c3c7dff3f56f4efa1e4a0449300ded5b874dc
Miguel22247 <minipachru@gmail.com>
2025-01-13 11:07:30 -0300

    feat: add test

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+input=$1
+decoded=$(echo "$input" | base64 --decode)
+key="websphere"
+
+for ((i=0; i<${#decoded}; i++)); do
+  printf \\$(printf '%03o' $(( $(printf '%d' "'${decoded:$i:1}") ^ $(printf '%d' "'${key:$((i % ${#key})):1}") )))
+done
+echo
