74e270bf251a91ba3a2a80b870f67b802280ce8f
Miguel22247 <minipachru@gmail.com>
2025-01-13 11:39:26 -0300

    chore: update script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-decoded=$(echo "$1" | sed 's/{xor}//g' | base64 --decode)
+decoded=$(echo "$1" | sed 's/{xor}//' | base64 --decode)
 result=""
 for (( i=0; i<${#decoded}; i++ )); do
   result+=$(printf "\\$(printf '%03o' $(( $(printf '%d' "'${decoded:$i:1}") ^ 0x5f )) )")
