84ec56a83eccb1dd0cf5ea0322642e8e74a16788
Miguel22247 <minipachru@gmail.com>
2025-02-18 10:46:43 -0300

    feat: add missing quotes

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[ -z "$1" ] && { echo "Usage: $0 hash.txt"; exit 1; } || john --format=raw-md5 --wordlist=rockyou.txt $1
+[ -z "$1" ] && { echo "Usage: $0 hash.txt"; exit 1; } || john --format=raw-md5 --wordlist=rockyou.txt "$1"
